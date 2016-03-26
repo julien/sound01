f0c06789ad8ffbe6aa5a340e4c7e5b05e6a3e71d
Julien C <jcastelain@gmail.com>
2016-03-26 01:19:43 +0100

    Add more particles, remove comments

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -9,10 +9,8 @@ int bandRadius =   10;
 int bandVelocity = 10;
 
 ofFbo fbo;
-
-const int numParticles = 500;
+const int numParticles = 5000;
 vector<Particle*> particles;
-ofVec3f angle;
 float time0 = 0;
 
 void ofApp::setup() {
@@ -24,7 +22,6 @@ void ofApp::setup() {
     sound.load("Spikenard.mp3");
     sound.play();
 
-    angle.set(ofRandom(0.5), 0, ofRandom(2.6));
 
     for (int i = 0; i < numParticles; i++) {
         Particle* p = new Particle();
@@ -40,17 +37,14 @@ void ofApp::update() {
     // ofSoundUpdate();
     float *val = ofSoundGetSpectrum(NBANDS);
     for (int i = 0; i < NBANDS; i++) {
-        spectrum[i] *= 0.98;
+        spectrum[i] *= ofRandom(0.95, ofRandom(0.98));
         spectrum[i] = max(spectrum[i], val[i]);
     }
 
     float time = ofGetElapsedTimef();
     float dt = time - time0;
-    dt = ofClamp(dt, 0.0, 0.5);
+    dt = ofClamp(dt, 0.0, 1.0);
     time0 = time;
-
-    // radius =   ofMap(spectrum[bandRadius], 1, 2, 400, 800, true);
-    // velocity = ofMap(spectrum[bandVelocity], 1, 0.4, 0.05, 0.25);
 
     for (int i = 0; i < particles.size(); i++) {
         Particle* p = particles.at(i);
@@ -62,13 +56,14 @@ void ofApp::update() {
         if (!p->alive()) {
             particles.erase(particles.begin() + i);
 
-            Particle* p = new Particle();
-            p->radius = ofRandom(ofGetWidth() / 2, ofGetWidth());
-            p->velocity.set(ofRandom(0.5), ofRandom(0.5));
-            p->offset.set(ofRandom(0, 1000), ofRandom(0, 1000));
-            p->size = ofRandom(4, 8);
-            particles.push_back(p);
-
+            if (i % 5 == 0) {
+                Particle* p = new Particle();
+                p->radius = ofRandom(ofGetWidth() / 2, ofGetWidth());
+                p->velocity.set(ofRandom(0.5), ofRandom(0.5));
+                p->offset.set(ofRandom(0, 1000), ofRandom(0, 1000));
+                p->size = ofRandom(4, 8);
+                particles.push_back(p);
+            }
         }
     }
 }
@@ -89,11 +84,6 @@ void ofApp::draw() {
         ofSetColor(0, 0, 0, p->getAlpha());
         ofDrawCircle(p->position, p->size);
     }
-
-    ofRotateX(angle.x);
-    ofRotateZ(angle.z);
-    angle.x += 1.5;
-    angle.z += 0.5;
 
     fbo.end();
     ofPopMatrix();
